16778227f3ca73222e9d559217d4fd83fff206e6
Dromediansk <pillar.mr@gmail.com>
2019-03-13 14:01:45 +0100

    adding age & pet into database

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined) values ('Jessie', 'jessie@gmail.com', 5, '2019-01-01');
+INSERT into users (name, age, pet, email, entries, joined) values ('Jessie', 25, 'dog','jessie@gmail.com', 5, '2019-01-01');
 INSERT into login (hash, email) values ('0cc175b9c0f1b6a831c399e269772661', 'jessie@gmail.com');
 
 COMMIT;
